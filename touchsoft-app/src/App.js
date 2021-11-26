f4f0af2a857b7237261c7a0c67d5cb3f3609630e
DmitryPashkou <86959216+DmitryPash@users.noreply.github.com>
2021-11-26 15:00:10 +0100

    functional component
    add counter

--- touchsoft-app/src/App.js
+++ touchsoft-app/src/App.js
@@ -1,53 +1,11 @@
 import React, { useEffect, useState } from "react";
 import "./style/App.css";
+import Counter from "./component/Counter";
 function App() {
-  const [count, setCount] = useState(0);
-  const [word, setWord] = useState("asd");
-  const [bcg, setBcg] = useState("counter");
-
-  useEffect(() => {
-    logica();
-  });
-
-  function logica() {
-    if (count % 2 == 0 && count !== 0) {
-      setBcg("counter even");
-      setWord("четное");
-    } else if (count % 2 !== 0 && count !== 0) {
-      setBcg("counter odd");
-      setWord("не четное");
-    } else {
-      setWord("0");
-    }
-  }
-
-  function Incr() {
-    setCount(count + 1);
-  }
-  function Dicr() {
-    if (count >= 1) {
-      setCount(count - 1);
-    }
-  }
-  function Reset() {
-    setCount(count - count);
-    setWord("0");
-    setBcg("counter");
-  }
-
+  const addNewCounter = () => {};
   return (
     <div className="App">
-      <div className="wrapper">
-        <div className={bcg}>{count}</div>
-        <div className="box-text">Введено {word} число</div>
-        <div className="btn-group">
-          <button onClick={Dicr}>Decriase</button>
-          <button onClick={Incr}>Increase</button>
-          <button onClick={Reset} className="btn-group-reset">
-            Reset
-          </button>
-        </div>
-      </div>
+      <Counter />
     </div>
   );
 }
--- touchsoft-app/src/component/counter.jsx
+++ touchsoft-app/src/component/counter.jsx
@@ -1,52 +1,54 @@
-// import React from "react";
+import React, { useEffect, useState } from "react";
 
-// function App() {
-//   const [count, setCount] = useState(0);
-//   const [word, setWord] = useState("asd");
-//   const [bcg, setBcg] = useState("counter");
+function Counter() {
+  const [count, setCount] = useState(0);
+  const [word, setWord] = useState("asd");
+  const [bcg, setBcg] = useState("counter");
 
-//   useEffect(() => {
-//     logica();
-//   });
+  useEffect(() => {
+    settingFunc();
+  });
 
-//   function logica() {
-//     if (count % 2 == 0 && count !== 0) {
-//       setBcg("counter even");
-//       setWord("четное");
-//     } else if (count % 2 !== 0 && count !== 0) {
-//       setBcg("counter odd");
-//       setWord("не четное");
-//     } else {
-//       setWord("0");
-//     }
-//   }
+  function settingFunc() {
+    if (count % 2 == 0 && count !== 0) {
+      setBcg("counter even");
+      setWord("четное");
+    } else if (count % 2 !== 0 && count !== 0) {
+      setBcg("counter odd");
+      setWord("не четное");
+    } else {
+      setWord("0");
+    }
+  }
 
-//   function Incr() {
-//     setCount(count + 1);
-//   }
-//   function Dicr() {
-//     if (count >= 1) {
-//       setCount(count - 1);
-//     }
-//   }
-//   function Reset() {
-//     setCount(count - count);
-//     setWord("0");
-//     setBcg("counter");
-//   }
-//   return (
-//     <div className="container">
-//       <div className="wrapper">
-//         <div className={bcg}>{count}</div>
-//         <div className="box-text">Введено {word} число</div>
-//         <div className="btn-group">
-//           <button onClick={Dicr}>Decriase</button>
-//           <button onClick={Incr}>Increase</button>
-//           <button onClick={Reset} className="btn-group-reset">
-//             Reset
-//           </button>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
+  function Incr() {
+    setCount(count + 1);
+  }
+  function Dicr() {
+    if (count >= 1) {
+      setCount(count - 1);
+    }
+  }
+  function Reset() {
+    setCount(count - count);
+    setWord("0");
+    setBcg("counter");
+  }
+  return (
+    <div className="container">
+      <div className="wrapper">
+        <div className={bcg}>{count}</div>
+        <div className="box-text">Введено {word} число</div>
+        <div className="btn-group">
+          <button onClick={Dicr}>Decriase</button>
+          <button onClick={Incr}>Increase</button>
+          <button onClick={Reset} className="btn-group-reset">
+            Reset
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Counter;
